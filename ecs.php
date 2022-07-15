5aa03667ab53a5e01b978ab88a2b76ea2dc6b2b2
Jakub Groncki <jakub.groncki@bitbag.pl>
2022-07-15 15:52:35 +0200

    Add QA tools

---
+++ ecs.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import('vendor/bitbag/coding-standard/ecs.php');
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        '',
+    ]);
+
+    $services = $containerConfigurator->services();
+    $services->remove(\Psalm\Issue\InvalidDocblock::class);
+};
